b9a8be92f9380b111f8e3bcdd10a4797e68d1e8c
Fu Yin <fooying@qq.com>
2021-09-26 19:44:37 +0800

    fix: components

--- src/Location/data.tsx
+++ src/Location/data.tsx
@@ -2,9 +2,11 @@ import { ILocationProps } from './index';
 
 export const locationDataSource: ILocationProps = {
   margin: '0px',
-  padding: '0px',
+  padding: '10px',
   borderRadius: 'none',
-  locationImage: '',
-  name: '',
+  locationImage: 'https://file.biji77.com/wechat/icon/location.png',
+  name: '武汉大学店',
   url: '',
+  chooseAddress: false,
+  chooseLocation: false,
 };
--- src/Location/index.tsx
+++ src/Location/index.tsx
@@ -9,12 +9,61 @@ export interface ILocationProps {
   borderRadius?: string;
   locationImage: string;
   name?: string;
-  url: string;
+  url?: string;
+  chooseLocation?: boolean;
+  chooseAddress?: boolean;
 }
 
 export const YYLocation = (props: ILocationProps) => {
-  const { margin = '0px', padding = '0px', borderRadius = '0px', locationImage, name, url } = props;
+  const {
+    margin = '0px',
+    padding = '0px',
+    borderRadius = '0px',
+    locationImage,
+    name,
+    url,
+    chooseAddress,
+    chooseLocation
+  } = props;
 
+  const handlePosition = () => {
+    if (url) {
+      router.navigateTo({
+        url,
+      });
+    } else if (chooseLocation) {
+      Taro.getLocation({
+        type: 'wgs84',
+        success: function (res) {
+          // const latitude = res.latitude
+          // const longitude = res.longitude
+          // const speed = res.speed
+          // const accuracy = res.accuracy
+          console.log('res getLocation', res)
+          Taro.chooseLocation({
+            complete: (res) => {console.log('complete res', res)},
+            fail: (res) => {console.log('fail res', res)},
+            latitude: res.latitude,
+            longitude: res.longitude,
+            success: (res) => {console.log('success res', res)}, 
+          })
+        }
+      })
+    } else if (chooseAddress) {
+      Taro.chooseAddress({
+        success: function (res) {
+          console.log(res.userName);
+          console.log(res.postalCode);
+          console.log(res.provinceName);
+          console.log(res.cityName);
+          console.log(res.countyName);
+          console.log(res.detailInfo);
+          console.log(res.nationalCode);
+          console.log(res.telNumber);
+        },
+      });
+    }
+  }
   return (
     <View
       className="yy-location-content"
@@ -23,29 +72,10 @@ export const YYLocation = (props: ILocationProps) => {
         padding,
         borderRadius,
       }}
-      onClick={() => {
-        if (url) {
-          router.navigateTo({
-            url,
-          });
-        } else {
-          Taro.chooseAddress({
-            success: function (res) {
-              console.log(res.userName);
-              console.log(res.postalCode);
-              console.log(res.provinceName);
-              console.log(res.cityName);
-              console.log(res.countyName);
-              console.log(res.detailInfo);
-              console.log(res.nationalCode);
-              console.log(res.telNumber);
-            },
-          });
-        }
-      }}
     >
       <Image className="yy-location-content_icon" src={locationImage} />
       {name && <View className="yy-location-content_text">{name}</View>}
+      <View className="yy-location-content_position" onClick={handlePosition}>重新定位</View>
     </View>
   );
 };
